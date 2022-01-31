af3b871449aecc90d375f05f20a9e2df57702247
Douglas Akamine <douglasakamine@gmail.com>
2022-01-31 09:48:34 +0900

    corrected delete function and created filter function in controller

--- src/app/Http/Controllers/BooksController.php
+++ src/app/Http/Controllers/BooksController.php
@@ -3,24 +3,47 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use App\Models\Books;
+use Illuminate\Support\Facades\Log;
 
 class BooksController extends Controller
 {
-    public function saveBook() {
+    public function getBooks(Request $request) {
 
-        $update = new Books;
-        $update->title = "test";
-        $update->author = "test2";
-        $update->save();
+        $booksQuery = Books::select('id','title','author');
+        $searchCriteria = $request->input('searchCriteria');
 
-    }
-
-    public function getBooks() {
-
-        $queryGetBooks = Books::select('id','title','author');
-        $result = $queryGetBooks->get();
+        if($searchCriteria !== null) {
+            $booksQuery = $this->filterBySearchCriteria($booksQuery, $searchCriteria);
+        }
+        $result = $booksQuery->get();
         return $result;
     }
+
+    public function saveBook(Request $request) {
+
+        $update = new Books;
+        $update->title = $request->input('title');
+        $update->author = $request->input('author');
+        $update->save();
+
+        return $request;
+    }
+
+    public function deleteBook(string $id) {
+
+        $result = Books::where('id', $id)->delete();
+        return $result;
+    }
+
+    private function filterBySearchCriteria($query, $searchCriteria) {
+
+        $filteredQuery = $query->where('title', 'LIKE', "%{$searchCriteria}%")
+            ->orWhere('author', 'LIKE', "%{$searchCriteria}%");
+        return $filteredQuery;
+    }
+
+
 
 }
